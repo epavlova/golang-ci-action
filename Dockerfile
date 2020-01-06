9056497c017152419b5199ec6a6aaa15963abb74
Elitsa Pavlova <elitsa.pavlova@ft.com>
2020-01-06 13:22:37 +0200

    Add testing and coverage report

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM golang:1.13
+FROM golang:1
 
 # install golangci linters' runner
-RUN curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b $(go env GOPATH)/bin v1.18.0
+RUN curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b $(go env GOPATH)/bin $INPUT_GOLANGCIVERSION
+
+# install goveralls and go-junit-report
+RUN GO111MODULE=off go get github.com/mattn/goveralls
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,15 @@
 go build -mod=readonly -v
 
 # download linters' config
-wget https://raw.githubusercontent.com/Financial-Times/upp-coding-standard/v1.0.0/golangci-config/.golangci.yml
+wget https://raw.githubusercontent.com/Financial-Times/upp-coding-standard/$INPUT_GOLANGCICONFIGVERSION/golangci-config/.golangci.yml
 
 # run linters'
 golangci-lint run --new-from-rev=$(git rev-parse origin/master) --config .golangci.yml --build-tags=integration
 
+mkdir /tmp/coverage-results
+
 # run tests
-go test ./...
+go test -tags=integration -mod=readonly -race -cover -coverprofile=/tmp/coverage-results/coverage.out ./...
+
+# report coverage
+goveralls -coverprofile=/tmp/coverage-results/coverage.out -service=circle-ci -repotoken=$INPUT_COVERALLSTOKEN
